9832ce9d6bb92777f8faad9c5c3ebe113ac50eb8
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-09-20 10:21:53 +0500

    Task2

---
+++ Tyuiu.MkhitaryanSG.Sprint0.Task2.V0.Lib/DataServise.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MkhitaryanSG.Sprint0.Task2.V0.Lib
+{
+    public class DataServise
+    {
+        public static string GetMessage(string name)
+        {
+            return $"Привет, {name}"
+        }
+
+    }
+}
